d21635f76f11c93884b8daeb058a08fb8a6bc5b5
Ilya Chekasov <51474856+IlyaCherkasov1@users.noreply.github.com>
2024-02-13 11:39:00 +0300

    set up initial infrastructure

---
+++ Backend/EnglishTutorAI.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EnglishTutorAI.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly));
+
+        return services;
+    }
+}
