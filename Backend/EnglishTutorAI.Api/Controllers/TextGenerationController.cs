16493680515f5ae3a103144490f4dfb666ab3ee5
Ilya Chekasov <51474856+IlyaCherkasov1@users.noreply.github.com>
2024-04-24 17:28:01 +0300

    add

---
+++ Backend/EnglishTutorAI.Api/Controllers/TextGenerationController.cs
@@ -0,0 +1,28 @@
+﻿using EnglishTutorAI.Api.Constants;
+using EnglishTutorAI.Api.Controllers.Attributes;
+using EnglishTutorAI.Application.Handlers.GenerateSentences;
+using EnglishTutorAI.Application.Models.TextGeneration;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishTutorAI.Api.Controllers;
+
+[ApiController]
+[ApiRoute]
+public class TextGenerationController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public TextGenerationController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost(Routes.Assistant.GenerateChatCompletion)]
+    public async Task<IActionResult> GenerateChatCompletionCommand(TextGenerationRequest request)
+    {
+        var result = await _mediator.Send(new GenerateChatCompletionCommand(request));
+
+        return Ok(result);
+    }
+}
---
+++ Backend/EnglishTutorAI.Api/ServiceInstallers/DependencyRegistrationsInstaller.cs
@@ -0,0 +1,23 @@
+﻿using EnglishTutorAI.Api.Interfaces;
+using EnglishTutorAI.Application.Interfaces;
+using EnglishTutorAI.Application.Services;
+using EnglishTutorAI.Application.Services.Sentences;
+using EnglishTutorAI.Infrastructure.Data;
+using EnglishTutorAI.Infrastructure.HttpClients;
+
+namespace EnglishTutorAI.Api.ServiceInstallers;
+
+public class DependencyRegistrationsInstaller : IServiceInstaller
+{
+    public void InstallServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<ISentenceRetrieverService, SentenceRetrieverService>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped<IOpenAiService, OpenAiService>();
+        services.AddSingleton<IHttpClientFactory, ConfigurableProxyHttpClientFactory>();
+        services.AddScoped<IPromptTemplateService, PromptTemplateService>();
+        services.AddScoped<IStoryService, StoryService>();
+        services.AddScoped<IStoryCounterService, StoryCounterService>();
+    }
+}
