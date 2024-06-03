26853534575e5aa93d12f818db46b203ae57b473
Ilya Chekasov <51474856+IlyaCherkasov1@users.noreply.github.com>
2024-06-03 09:29:03 +0300

    improvement of text comparison method

---
+++ Backend/EnglishTutorAI.Application/Services/TextComparisonService.cs
@@ -0,0 +1,21 @@
+﻿using EnglishTutorAI.Application.Interfaces;
+
+namespace EnglishTutorAI.Application.Services;
+
+public class TextComparisonService : ITextComparisonService
+{
+    private static readonly char[] Separator = [' ', '.', ',', ';', ':', '!', '?'];
+
+    public bool HasTextChanged(string originalText, string correctedText)
+    {
+        var originalWords = originalText.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
+        var correctedWords = correctedText.Split(Separator, StringSplitOptions.RemoveEmptyEntries);
+
+        if (originalWords.Length != correctedWords.Length)
+        {
+            return true;
+        }
+
+        return originalWords.Where((t, i) => !t.Equals(correctedWords[i], StringComparison.OrdinalIgnoreCase)).Any();
+    }
+}
