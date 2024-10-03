6f3b7af3acbdc94d94d9f3115ca56d35cc8857e5
Ilya Chekasov <51474856+IlyaCherkasov1@users.noreply.github.com>
2024-10-03 16:55:59 +0300

    Add usedImplicitly attribute to custom DependencyAttribute

--- server/EnglishTutorAI.Application/Attributes/DependencyAttribute.cs
+++ server/EnglishTutorAI.Application/Attributes/DependencyAttribute.cs
@@ -1,8 +1,10 @@
 ﻿using EnglishTutorAI.Domain.Enums;
+using JetBrains.Annotations;
 
 namespace EnglishTutorAI.Application.Attributes;
 
 [AttributeUsage(AttributeTargets.Class)]
+[UsedImplicitly(ImplicitUseKindFlags.Assign)]
 public class DependencyAttribute : Attribute
 {
     public DependencyLifetime Lifetime { get; }
