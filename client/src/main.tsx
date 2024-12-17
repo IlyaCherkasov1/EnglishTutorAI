9b0e1079db21aaf5b7b6a402e7eca1bc14e8bafc
Ilya Chekasov <51474856+IlyaCherkasov1@users.noreply.github.com>
2024-12-17 15:41:38 +0300

    add error Boundary

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,10 @@ import {createRoot} from 'react-dom/client'
 import '@/index.css'
 import '@/i18n.ts'
 import App from "@/app.tsx";
+import {ErrorBoundary} from "@/app/components/errorBoundary.tsx";
 
 createRoot(document.getElementById('root')!).render(
-    <App/>
+    <ErrorBoundary >
+        <App />
+    </ErrorBoundary>
 )
