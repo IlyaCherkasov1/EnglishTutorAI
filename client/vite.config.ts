2e16ca29325a6f75dbc5719b5008626335cea574
Ilya Chekasov <51474856+IlyaCherkasov1@users.noreply.github.com>
2025-01-18 16:10:04 +0300

    update node version

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,20 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 import path from 'path';
 import checker from "vite-plugin-checker";
+
+const isCI = process.env.CI === 'true';
 
 export default defineConfig({
     plugins: [react(), checker({ typescript: true })],
     server: {
+        https: !isCI
+            ? {
+                key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+                cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+            }
+            : undefined,
     },
     resolve: {
         alias: {
