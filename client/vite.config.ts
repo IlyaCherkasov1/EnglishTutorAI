ab4d14d7547615d31d1acdbaac99cd917896a7f9
Ilya Chekasov <51474856+IlyaCherkasov1@users.noreply.github.com>
2025-01-19 19:16:38 +0300

    test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ import checker from "vite-plugin-checker";
 const isCI = process.env.CI === 'true';
 
 export default defineConfig({
-    base: './',
+    base: '/',
     plugins: [react(), checker({ typescript: true })],
     server: {
         https: !isCI
